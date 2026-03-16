35ad6c9bcb7c29832a11973896815f76f8a2a61f
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-03-16 14:06:46 +0600

    :art: Style: Added Backgorund picture in Hero section and make the text bigger

--- app/components/sections/Hero.tsx
+++ app/components/sections/Hero.tsx
@@ -3,42 +3,45 @@ import {Button} from 'antd';
 
 export default function Hero(){
     return(
-        <div id="Hero" className="flex flex-col w-full bg-blue-100 items-center">
-            <div className="flex flex-col py-10 gap-5 items-center justify-center">
-                <h1 className="font-david py-2 lg:py-2 font-bold text-xl lg:text-2xl text-blue-900  text-center">Emergency  <span className="text-xl text-red-600 font-mono "> 24/7 </span>plumbing service at your door</h1>
-                <p className="font-david   font-bold text-xl lg:text-2xl text-blue-900  text-center"><span className="text-xl lg:text-2xl text-red-600 font-mono">24</span> Hours Service</p>
-                <p className="font-david   font-bold text-xl lg:text-2xl text-blue-900  text-center"> <span className="text-xl text-red-600 font-mono">30-60</span> Minutes Arrival Time</p>
-                
-                    <Button type="primary" 
-                      size="large"
-                      href="tel:01793229730"
-                      className="bg-orange-700!  w-[70%] md:w-full h-15!  text-3xl! md:text-4xl! text-white ">
-                        01-0100-101
-                    </Button>
-                    
-                
-            </div>
+     <div id="Hero" className="relative flex flex-col bg-[url('/HeroBG.png')] bg-cover bg-center h-screen items-center pt-20 ">
+         
+         <div className="absolute inset-0 bg-black/40"></div>
+         <div className="relative flex flex-col py-10 gap-5 items-center justify-center z-10">
+           <h1 className="font-david py-2 lg:py-2 font-bold text-xl md:text-2xl lg:text-4xl text-white text-center">
+             Emergency <span className="text-xl lg:text-2xl text-red-600 font-mono">24/7</span> plumbing service at your door
+           </h1>
+       
+           <p className="font-david font-bold text-xl md:text-2xl lg:text-4xl text-white text-center">
+             <span className="text-xl lg:text-2xl text-red-600 font-mono">24</span> Hours Service
+           </p>
+       
+           <p className="font-david font-bold text-xl md:text-2xl lg:text-4xl text-white text-center">
+             <span className="text-xl lg:text-2xl text-red-600 font-mono">30-60</span> Minutes Arrival Time
+           </p>
+       </div>
+       
+
 
             <div className="flex flex-wrap items-center py-4 px-6 md:px-10 gap-3">
-               <span className="rounded-full bg-blue-300 px-4 py-2 text-sm md:text-base font-serif text-blue-900 shadow-sm">
+               <span className="rounded-full bg-blue-400 px-4 py-2 text-md md:text-lg font-serif text-blue-900 shadow-sm">
                  Licensed
                </span>
-               <span className="rounded-full bg-blue-300 px-4 py-2 text-sm md:text-base font-serif text-blue-900 shadow-sm">
+               <span className="rounded-full bg-blue-400 px-4 py-2 text-md md:text-lg font-serif text-blue-900 shadow-sm">
                  Insured
                </span>
-               <span className="rounded-full bg-blue-300 px-4 py-2 text-sm md:text-base font-serif text-blue-900 shadow-sm">
+               <span className="rounded-full bg-blue-400 px-4 py-2 text-md md:text-lg font-serif text-blue-900 shadow-sm">
                  20+ years
                </span>
             </div>
             
             <div className="flex flex-wrap items-center py-4 px-6 md:px-10 gap-3">
-               <span className="rounded-full bg-blue-300 px-4 py-2 text-sm md:text-base font-serif text-blue-900 shadow-sm">
+               <span className="rounded-full bg-blue-400 px-4 py-2 text-md md:text-lg font-serif text-blue-900 shadow-sm">
                  Serving Fresno
                </span>
-               <span className="rounded-full bg-blue-300 px-4 py-2 text-sm md:text-base font-serif text-blue-900 shadow-sm">
+               <span className="rounded-full bg-blue-400 px-4 py-2 text-md md:text-lg font-serif text-blue-900 shadow-sm">
                  CA
                </span>
-               <span className="rounded-full bg-blue-300 px-4 py-2 text-sm md:text-base font-serif text-blue-900 shadow-sm">
+               <span className="rounded-full bg-blue-400 px-4 py-2 text-md md:text-lg font-serif text-blue-900 shadow-sm">
                  Badge
                </span>
             </div>
