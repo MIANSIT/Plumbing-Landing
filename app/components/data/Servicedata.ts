c81b99386169834c82882e4e62791791a0a84491
mdrifatunnobirifat <mdrifatunnabi@gmail.com>
2026-03-28 13:31:19 +0600

    :art: Style: Chnage the style of the serivice Cards

--- app/components/data/Servicedata.ts
+++ app/components/data/Servicedata.ts
@@ -4,10 +4,10 @@ import Image from "next/image";
 import ServiceCard from "../ui/ServicesCard";
 
 export const Servicedata=[
-    {id:"1",icon:"/ServiceWatericon.png",heading:"Emergency Repairs",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
-    {id:"2",icon:"/ServiceWatericon.png",heading:"Drain Cleaning",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
-    {id:"3",icon:"/ServiceWatericon.png",heading:"Water Heater Installation",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
-    {id:"4",icon:"/ServiceWatericon.png",heading:"Leak Detection",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
-    {id:"5",icon:"/ServiceWatericon.png",heading:"Bathroom Remodeling",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
-    {id:"6",icon:"/ServiceWatericon.png",heading:"Commercial Plumbing",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
+    {id:"1",icon:"/PipeLeaking.png",heading:"Emergency Repairs",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
+    {id:"2",icon:"/CloggedDrains.png",heading:"Drain Cleaning",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
+    {id:"3",icon:"/WaterRun.png",heading:"Water Heater Installation",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
+    {id:"4",icon:"/PipeLeaking.png",heading:"Leak Detection",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
+    {id:"5",icon:"/PipeLeaking.png",heading:"Bathroom Remodeling",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
+    {id:"6",icon:"/WaterRun.png",heading:"Commercial Plumbing",deatils:"fixing the atwe cnhf cnd cnhfid dncufh",more:"Call Now"},
 ];
--- app/components/ui/ServicesCard.tsx
+++ app/components/ui/ServicesCard.tsx
@@ -11,30 +11,35 @@ interface Cardprops{
 export default function ServiceCard({icon,heading,details,more}:Cardprops){
 
     return(
-        <div className=" slide-in flex flex-col items-center gap-5 p-5 bg-transparent rounded-xl shadow-md hover:bg-blue-200 hover:-translate-y-2 transition-all duration-300 text-left">
+        <div className="group relative h-64 slide-in flex flex-col items-center justify-center p-5 rounded-xl shadow-md overflow-hidden transition-all duration-300 hover:-translate-y-2">
 
-            <div className= "relative w-16 h-16 ">
-                <Image
-                    src={icon}
-                    alt="icon"
-                    fill
-                    className="object-contain"
-                    />
+          
+            <Image
+                src={icon}
+                alt="icon"
+                fill
+                className="object-cover opacity-90"
+            />
+
+           
+            <div className="absolute inset-0 flex flex-col items-center justify-center gap-3 bg-blue-200/80 opacity-0 transition-all duration-300 group-hover:opacity-100 text-center p-4">
+                
+                <div className="drop-inHeading text-3xl text-blue-900 font-['Inter']">
+                    {heading}
+                </div>
+
+                <div className="drop-inButton text-blue-600 font-medium cursor-pointer">
+                    <Button
+                        href="tel:01793229730"
+                        type="primary"
+                        size="large"
+                        className="bg-red-500! text-white px-8 py-4"
+                    >
+                        {more}
+                    </Button>
+                </div>
             </div>
-            <div className=" drop-inHeading text-2xl text-blue-700 font-['Inter'] ">
-                {heading}
-            </div>
-            <div className="text-gray-600 text-sm ">
-                {details}
-            </div>
-            <div className=" drop-inButton text-blue-600 font-medium cursor-pointer hover:underline">
-                <Button
-                href="tel:01793229730"
-                type="primary"
-                size="large"
-                className="bg-red-400! text-white px-8 py-4">{more}</Button>
-               
-            </div>
+
         </div>
     );
 }
